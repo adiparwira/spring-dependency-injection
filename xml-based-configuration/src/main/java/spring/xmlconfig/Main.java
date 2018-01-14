ed3a3b858d9fe3c59f29f094e0c1b1bba6c86121
Aryadi <adiparwira@yahoo.com>
2018-01-14 14:03:03 +0700

    add xml-based-configuration

---
+++ xml-based-configuration/src/main/java/spring/xmlconfig/Main.java
@@ -0,0 +1,30 @@
+package spring.xmlconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.bean.AccountService;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+
+		AccountService accountService = context.getBean(AccountService.class);
+
+		System.out.println("Before money transfer");
+		System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+
+		accountService.transferMoney(1, 2, 5.0);
+
+		System.out.println("After money transfer");
+		System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+
+		((ConfigurableApplicationContext) (context)).close();
+
+	}
+
+}
